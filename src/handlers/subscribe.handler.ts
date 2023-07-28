249bb80cbf6d7d905e548425746b230480f54606
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2023-07-28 21:54:22 +0100

    Feat: updated the cron job and subscribe

--- src/handlers/subscribe.handler.ts
+++ src/handlers/subscribe.handler.ts
@@ -1,7 +1,22 @@
-import job from "../services/cron-job.service";
+import TelegramBot, { Message } from "node-telegram-bot-api";
+import UserModel from "../models/user.model";
 
-const subscribe = () => {
-	job.start();
+const subscribe = async (message: Message, bot: TelegramBot) => {
+	const user_id = message.chat.id.toString();
+	const user = await UserModel.findOne({ user_id });
+	try {
+		if (user) {
+			await UserModel.findOneAndUpdate(
+				{ user_id },
+				{ subscribed: true },
+				{ new: false }
+			);
+			const message = `You are now subscribed to receive a daily dose of dry humor every morning! 🌞\nStarting from tomorrow morning, our bot will send you a witty and hilarious dry joke to brighten up your day. 😂\nBe prepared to laugh out loud every day as you receive your daily joke in the morning. 🎉\nThank you for subscribing! If you ever want to unsubscribe, just use the /unsubscribe command.`;
+			bot.sendMessage(user_id, message);
+		}
+	} catch (error) {
+		console.error("User could not be subscribed", error);
+	}
 };
 
 export default subscribe;
--- src/services/cron-job.service.ts
+++ src/services/cron-job.service.ts
@@ -1,12 +1,40 @@
 import cron from "cron";
+import UserModel from "../models/user.model";
+import TelegramBot from "node-telegram-bot-api";
 
-// Your function to be run every 20 seconds
-const myFunction = () => {
-	console.log("This function will be run every 20 seconds.");
+const sendMessageToUser = (
+	bot: TelegramBot,
+	chatId: string,
+	message: string
+) => {
+	// bot.sendMessage(chatId, message);
+	// console.log(message);
+};
+
+const sendMessagesToSubscribedUsers = async (bot: TelegramBot) => {
+	try {
+		const subscribedUsers = await UserModel.find({ subscribed: true });
+
+		subscribedUsers.forEach((user) => {
+			const chatId = user.user_id.toString();
+			const message = `You are now subscribed to receive a daily dose of dry humor every morning! 🌞\nStarting from tomorrow morning, our bot will send you a witty and hilarious dry joke to brighten up your day. 😂\nBe prepared to laugh out loud every day as you receive your daily joke in the morning. 🎉\nThank you for subscribing! If you ever want to unsubscribe, just use the /unsubscribe command.`;
+			sendMessageToUser(bot, chatId, message);
+		});
+
+		console.log("Messages sent to subscribed users.");
+	} catch (error) {
+		console.error("Error sending messages:", error);
+	}
 };
 
 // Schedule the CRON job to run every 20 seconds
-const job = new cron.CronJob("*/5 * * * * *", myFunction);
+const createCronJob = (bot: TelegramBot) => {
+	const job = new cron.CronJob("*/5 * * * * *", () => {
+		sendMessagesToSubscribedUsers(bot);
+	});
 
-// Start the CRON job
-export default job;
+	return job;
+};
+// const job = new cron.CronJob("0 9 * * *", myFunction);
+
+export default createCronJob;
